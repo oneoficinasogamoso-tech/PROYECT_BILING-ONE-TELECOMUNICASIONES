2fd99a48fad78f5fe45ed1e6f3a55cd0e074d77f
david martinez <davidmartinez030404@gmail.com>
2026-01-23 13:05:50 -0500

    dtabase

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ class Config:
     SECRET_KEY = '678908766789087'
 
     # Database configuration
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '45632341'
-    MYSQL_DB = 'one_telecomunicasiones'
+    MYSQL_HOST = 'bayhr8qklsiu6zqznnwh-mysql.services.clever-cloud.com'
+    MYSQL_USER = 'uwioqepbquygtlkr'
+    MYSQL_PASSWORD = '7INuvP2DNU3FpRcJCdoc'
+    MYSQL_DB = 'bayhr8qklsiu6zqznnwh'
+    MYSQL_PORT = 3306
